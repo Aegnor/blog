482c2a2372636e499ce9d700f7a25cc322bf11f2
SBRSV <12alexman@gmail.com>
2021-06-26 09:42:06 +0300

    creaate routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostDetailPageComponent } from './post-detail-page/post-detail-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/posts', pathMatch: 'full'},
+      {path: 'posts', component: HomePageComponent, canActivate: [AuthGuard]},
+      {path: 'post/:id', component: PostDetailPageComponent, canActivate: [AuthGuard]}
+    ]
+  },
+  {
+    path: 'login', component: LoginPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
